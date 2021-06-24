41110b1f00212228d692a3ab0f0e26b1fa716955
shotaoba <84951023+shotaoba@users.noreply.github.com>
2021-06-24 16:26:49 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday,        presence: true
+  validates :nickname,          presence: true
+  validates :last_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name,        presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana,    presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana,   presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :birthday,          presence: true
+
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password,presence: true, format: {with: PASSWORD_REGEX}
+
 
 end
