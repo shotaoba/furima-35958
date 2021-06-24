52776c0c45ad63357b58ea79e00b6e8525ad8b4d
shotaoba <84951023+shotaoba@users.noreply.github.com>
2021-06-24 20:19:12 +0900

    ActiveStorageとActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :roduct_status_id
+    validates :shipping_charges_id
+    validates :prefecture_id
+    validates :shipping_date_and_time_id
+    validates :price
+  end
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
 end
