d33b97296e1f1e988f5af8907d56e56e346f86ef
shotaoba <84951023+shotaoba@users.noreply.github.com>
2021-06-25 20:49:18 +0900

    プルリクエストの修正4

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -59,17 +59,17 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
       it "priceが¥300未満だと登録できない" do
-        @item.price  = '299'
+        @item.price  = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
       it "priceが¥99,999,999円より大きいだと登録できない" do
-        @item.price  = '1000000000'
+        @item.price  = 1000000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
       it "priceが全角数字だと登録できない" do
-        @item.price  = '１２３４５'
+        @item.price  = １２３４５
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
@@ -84,27 +84,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
       it "category_idが1だと登録できない" do
-        @item.category_id  = '1'
+        @item.category_id  = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it "product_status_idが1だと登録できない" do
-        @item.product_status_id  = '1'
+        @item.product_status_id  = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Product status must be other than 1")
       end
       it "shipping_charges_idが1だと登録できない" do
-        @item.shipping_charges_id  = '1'
+        @item.shipping_charges_id  = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping charges must be other than 1")
       end
       it "prefecture_idが1だと登録できない" do
-        @item.prefecture_id  = '1'
+        @item.prefecture_id  = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it "shipping_date_and_time_idが1だと登録できない" do
-        @item.shipping_date_and_time_id  = '1'
+        @item.shipping_date_and_time_id  = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping date and time must be other than 1")
       end
