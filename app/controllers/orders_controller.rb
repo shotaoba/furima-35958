390bc080959e1a8392e320c28f92383ef4bc1623
shotaoba <84951023+shotaoba@users.noreply.github.com>
2021-06-30 11:47:31 +0900

    条件別画面の遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :move_to_index, only: [:index, :create]
+  before_action :authenticate_user!
 
   def index
     @item = Item.find(params[:item_id])
@@ -18,5 +20,9 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture, :municipalities, :building, :address, :phone_number ).merge(user_id: current_user.id ,item_id: params[:item_id])
+  end
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if user_signed_in? && current_user.id == @item.user_id
   end
 end
