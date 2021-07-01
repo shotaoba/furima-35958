328a778a6eef90e66730c8534ebf140b5f2a3c0a
shotaoba <84951023+shotaoba@users.noreply.github.com>
2021-07-01 15:33:19 +0900

    プルリクエストの修正3

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_order, only: [:index, :create]
   before_action :move_to_index, only: [:index, :create]
-  
+
   def index
     @order_address = OrderAddress.new
   end
@@ -10,12 +10,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -35,5 +30,13 @@ class OrdersController < ApplicationController
   end
   def set_order
     @item = Item.find(params[:item_id])
+  end
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
